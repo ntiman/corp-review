3977872f9f3a1574c3eca02683fef176e1f914cf
ntiman <ntiman1996@gmail.com>
2024-03-25 23:59:28 -0400

    Character limiter for review textarea

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+
+  const charCount = MAX_CHARACTERS - text.length;
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = event.target.value;
+    if (newText.length > MAX_CHARACTERS) return;
+    setText(newText);
+  };
+
   return (
     <form className="form">
-      <textarea id="feedbach-textarea" placeholder="blabla" spellCheck={false}></textarea>
+      <textarea
+        value={text}
+        onChange={handleChange}
+        id="feedbach-textarea"
+        placeholder="blabla"
+        spellCheck={false}
+      ></textarea>
 
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
 
       <div>
-        <p className="u-italic">150</p>
-        <button type="submit" className=""><span>Submit</span></button>
+        <p className="u-italic">{charCount}</p>
+        <button type="submit" className="">
+          <span>Submit</span>
+        </button>
       </div>
-
     </form>
-  )
+  );
 }
